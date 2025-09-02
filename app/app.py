3da5d856c88ee8369ff8435e6e2dd92caa6262d0
Fabio Bartoli <fabio.bartoli-ext@dex.co>
2025-09-01 21:42:44 -0300

    ci: add healthcheck and fix Dockerfile to new chainguard version

--- app/app.py
+++ app/app.py
@@ -190,5 +190,9 @@ def guarantee():
 
     return jsonify({'status': 'success'})
 
+@app.route('/health')
+def health():
+    return jsonify({'status': 'healthy'}), 200
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
